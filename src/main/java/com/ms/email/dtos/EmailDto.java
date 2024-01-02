a43f492a728146372172491459d259fc514cf3b0
Vinicius Brisotti <vinicius.brisotti@hotmail.com>
2024-01-02 01:54:24 -0300

    Creating Email DTO

---
+++ src/main/java/com/ms/email/dtos/EmailDto.java
@@ -0,0 +1,12 @@
+package com.ms.email.dtos;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public record EmailDto(@NotBlank String ownerRef, @NotBlank @Email String emailFrom, @NotBlank @Email String emailTo, @NotBlank String subject, @NotBlank String text) {
+
+}
